8021d5130a423e720d7bd09203fd7ee35146f439
Shan <shannon.mulgrew2@mail.dcu.ie>
2024-06-22 22:10:38 +0100

    adding in get request to backend project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'whimsy';
+export class AppComponent implements OnInit {
+  title = 'my-app';
+  data: any;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.apiService.getData().subscribe(
+      response => {
+        this.data = response;
+        console.log(this.data);
+      },
+      error => {
+        console.error('Error fetching data', error);
+      }
+    );
+  }
 }
